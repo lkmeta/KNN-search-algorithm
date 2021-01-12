f064618abf7bb22ca846a21b6881de043560e841
Kosmas-Louis Meta <74689374+lkmeta@users.noreply.github.com>
2021-01-12 03:05:22 +0200

    Update sequential.sh

--- bash scripts/sequential.sh
+++ bash scripts/sequential.sh
@@ -17,7 +17,7 @@ echo "v0 results" > ./results/v0.txt
 for file in ./matrices/*; do
 	echo "" >> ./results/v0.txt
 	echo "Running v0 for matrix: "$file >> ./results/v0.txt
-    for k in 5 10 20 30; do
+    for k in 5 10 20 50 100; do
 		./v0 $file $i >> ./results/v0.txt
 	done
 	echo "" >> ./results/v0.txt
